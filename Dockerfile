6945bffc1df4c16ddbf2225a69728fb9c66f83e6
Mauro <mauro@insided.nl>
2015-11-09 00:22:51 +0100

    Better isolated container, just a PHP service now.

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,17 @@
 FROM maxexcloo/nginx-php
 VOLUME ["/data/http", "/data/config"]
+ADD ./src /data/http
+ADD ./config /data/config
+
+ENV DB_HOST localhost
+ENV DB_NAME auth
+ENV DB_PORT 5432
+ENV DB_USER root
+ENV DB_PASSWORD root
+
+RUN apt-get install -y curl && \
+    apt-get install -y make && \
+    curl -sS https://getcomposer.org/installer | php
+
 WORKDIR /data/http
-CMD ["make"]
+RUN make
--- src/propel.php
+++ src/propel.php
@@ -6,9 +6,9 @@ return [
                 'auth' => [
                     'adapter'    => 'pgsql',
                     'classname'  => 'Propel\Runtime\Connection\ConnectionWrapper',
-                    'dsn'        => 'pgsql:host=login_db_1;port=5432;dbname=auth;user=root;password=root',
-                    'user'       => 'root',
-                    'password'   => 'root',
+                    'dsn'        => 'pgsql:host='.$_ENV["DB_HOST"].';port='.$_ENV["DB_PORT"].';dbname='.$_ENV["DB_NAME"],
+                    'user'       => $_ENV["DB_USER"],
+                    'password'   => $_ENV["DB_PASSWORD"],
                     'attributes' => [],
                     'settings'   => [
                         'charset'   => 'utf8',
